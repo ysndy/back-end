b162f1e03f16a28d3ef6c2b9dadb12396a39d445
Ji-su-Shin <caujisu2020@gmail.com>
2024-01-11 01:01:17 +0900

    feat: file entity 생성

--- src/main/java/say/backend/domain/file/FileInfo.java
+++ src/main/java/say/backend/domain/file/FileInfo.java
@@ -1,4 +1,45 @@
 package say.backend.domain.file;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import say.backend.domain.common.DelYn;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@Getter @Setter @Entity
+@Table(name = "file_info")
 public class FileInfo {
+    @Column(name="file_idx")
+    @Id
+    private String fileIdx;
+
+    @Column(name="original_file_name")
+    private String originalFileName;
+
+    @Column(name="file_name")
+    private String fileName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="file_type")
+    private FileType fileType;
+
+    @Column(name="save_file_name")
+    private String saveFileName;
+
+    @Column(name="reg_dt")
+    private LocalDateTime regDt;
+
+    @Column(name="mod_dt")
+    private LocalDateTime modDt;
+
+    @ColumnDefault("'N'")
+    @Column(name="del_yn")
+    private DelYn delYn;
 }
