0884b137105babb84a7a83591001d56fc4f1f700
Ji-su-Shin <caujisu2020@gmail.com>
2024-01-11 01:20:54 +0900

    feat: file entitiy 생성

--- src/main/java/say/backend/domain/file/PlaceFile.java
+++ src/main/java/say/backend/domain/file/PlaceFile.java
@@ -1,4 +1,18 @@
 package say.backend.domain.file;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+
+import say.backend.domain.file.pk.PlaceFilePK;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@Getter @Setter @Entity
+@Table(name = "place_file")
 public class PlaceFile {
+    @EmbeddedId
+    private PlaceFilePK placeFilePK;
 }
--- src/main/java/say/backend/domain/file/ReportFile.java
+++ src/main/java/say/backend/domain/file/ReportFile.java
@@ -1,4 +1,18 @@
 package say.backend.domain.file;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+
+import say.backend.domain.file.pk.ReportFilePK;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@Getter @Setter @Entity
+@Table(name = "report_file")
 public class ReportFile {
+    @EmbeddedId
+    private ReportFilePK reportFilePK;
 }
